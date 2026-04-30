fae97c590a5955245d43425e24d14dee504c1865
yashavi-kariya <kariyayashvi01@gmail.com>
2026-04-30 11:26:34 +0530

    fix:admin dashboard

--- src/components/admin/AdminMessages.jsx
+++ src/components/admin/AdminMessages.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback, useRef } from "react";
-import api from "../api";
+import api from "../../api";
 
 const timeAgo = (d) => {
     const diff = Date.now() - new Date(d);
