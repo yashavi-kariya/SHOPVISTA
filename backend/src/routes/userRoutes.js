398878fd94b9f624cefa216535c219c1c8963272
yashavi-kariya <kariyayashvi01@gmail.com>
2026-05-04 12:19:51 +0530

    export default

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -16,7 +16,7 @@ router.get("/admin/users", protect, adminOnly, getAllUsers);
 router.delete("/admin/users/:id", protect, adminOnly, deleteUser);
 
 export default router;
-export default router;
+// export default router;
 // import express from "express";
 // import { loginUser, getProfile, getAllUsers, deleteUser } from "../controllers/userController.js";
 // import { protect, adminOnly } from "../middleware/authMiddleware.js";
